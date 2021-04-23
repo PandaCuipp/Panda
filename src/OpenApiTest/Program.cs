d7daf85a43edb7020cba65afd35c08936319d4ad
cuipp <cuipp@hytch.com>
2021-04-23 11:20:24 +0800

    SignalR

---
+++ src/OpenApiTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenApiTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var http = new HttpClient();
+            var client = new swaggerClient("http://localhost:5000/", http);
+            var result = await client.WeatherForecastAsync(1);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Date}, {item.TemperatureF}, {item.Summary}");
+            }
+            Console.WriteLine("Hello World!");
+
+            var result2 = await client.GetLenAsync(3);
+            foreach (var item in result2)
+            {
+                Console.WriteLine($"{item.Date}, {item.TemperatureF}, {item.Summary}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- src/WebApplication2/Program.cs
+++ src/WebApplication2/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace SignalRChat
+namespace OpenApiWeb
 {
     public class Program
     {
